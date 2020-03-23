0bdd5ce3d0d8c9874bbafb9d9a6f7d2fd027a08c
ThanhPhucHuynh <nguoigiaumat100@gmail.com>
2020-03-23 20:22:05 +0700

    gan xong

--- index.js
+++ index.js
@@ -4,15 +4,21 @@ const bodyParser = require('body-parser')
 const mongoose = require('mongoose');
 const multer = require('multer')
 const path = require('path');
-
+var cors = require('cors');
 const app = express();
 const URL = process.env.URL_DB;
 const PORT = process.env.PORT_SV
 
 const User=require('./models/user.model')
-var product = require("./models/product.model")
-
+const Product = require("./models/product.model")
+app.use(cors())
 // app.use(bodyParser.json());
+// app.use(function(req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD"); // update to match the domain you will make the request from
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+//   });
+
 app.use(bodyParser.json({limit: '50mb'}));
 app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 // app.use(bodyParser.urlencoded({ extended: true }));
@@ -73,8 +79,18 @@ app.get('/user/:userEmail' ,(req,res)=>{
         res.status(400).send(e);
     })
 })
+
+// app.get('/user/:userEmail' ,(req,res)=>{
+//     var userEmail = req.params.userEmail;
+//     console.log(userEmail)
+//     User.findOne({email:userEmail}).then((user)=>{
+//         res.send({user});
+//     }, (e)=>{
+//         res.status(400).send(e);
+//     })
+// })
 app.get('/product',(req,res)=>{
-    product.find().then((product)=>{
+    Product.find().then((product)=>{
         res.send({product});
     }, (e)=>{
         res.status(400).send(e);
@@ -93,6 +109,32 @@ app.post('/user', imgUser.single('userImg'),async (req,res)=>{
             });
             await user.save().then(user=>{
                 res.send(user)
+            },(e)=>{
+                res.status(400).send(e);
+            })
+           
+    // }catch(err){
+    //     console.log(err);
+    // }
+    
+})
+
+app.post('/product',async (req,res)=>{
+    // try{
+        console.log(req.file);
+        var product = new Product({
+            id: req.body.id,
+            name: req.body.name,
+            description: req.body.description,
+            price: req.body.price,
+            total: req.body.total,
+            img: req.body.img,
+           
+            });
+            console.log(product);
+            
+            await product.save().then(product=>{
+                res.send(product)
             },(e)=>{
                 res.status(400).send(e);
             })
--- models/product.model.js
+++ models/product.model.js
@@ -1,13 +1,15 @@
 const mongoose = require('mongoose');
 
 const productSchema = mongoose.Schema({
-    _id: mongoose.Schema.Types.ObjectId,
-    id: Number,
+    // _id: mongoose.Schema.Types.ObjectId,
+    id: String,
     name: String,
     description: String,
+    price: Number,
+    total: Number,
     img: String
 });
-module.exports = mongoose.model('books', productSchema,'books');
+module.exports = mongoose.model('product', productSchema);
  
 
   
